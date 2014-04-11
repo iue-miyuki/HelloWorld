e6e8ea4c9dce9e23b92fdcc8c291f84bf8a0a7cf
g12910hm <g12910hm@s30416.tsuda.ac.jp>
2014-04-11 11:04:39 +0900

    新規にhello作りました。

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author g12910hm
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+}
